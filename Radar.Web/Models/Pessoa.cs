719b7a8c518f72e2d1924e2e601733e4c8e85aa4
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2023-10-23 16:19:07 -0300

    Add models

---
+++ Radar.Web/Models/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿namespace Radar.Web.Models
+{
+    public class Pessoa
+    {
+        public int PessoaID { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Login { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public DateTimeOffset DataNascimento { get; set; }
+    }
+}
