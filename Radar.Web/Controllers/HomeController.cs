43af20e2f1f77592df56fc4e7000ac85bca15f75
dibinvader <74374017+dibinvader@users.noreply.github.com>
2023-10-23 22:28:33 -0300

    Criação da tela Home e ajuste na estrutura de pastas do projeto

--- Radar.Web/Controllers/HomeController.cs
+++ Radar.Web/Controllers/HomeController.cs
@@ -25,29 +25,5 @@ namespace Radar.Web.Controllers
         {
             return View();
         }
-
-        public IActionResult Profile()
-        {
-            ProfileViewModel profileViewModel = new ProfileViewModel
-            (
-                pessoa: new Pessoa()
-                {
-                    PessoaID = 1,
-                    Nome = "Thainá",
-                    Email = "thaina@gmail.com",
-                    Login = "Thainá",
-                    Senha = "123456",
-                    DataNascimento = DateTimeOffset.Now
-                },
-                posts: ApiClient.GetPosts()
-            );
-            return View(profileViewModel);
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ Radar.Web/Controllers/ProfileController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Radar.Web.Api;
+using Radar.Web.Models.ViewModels;
+using Radar.Web.Models;
+using System.Diagnostics;
+
+namespace Radar.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ApiClient ApiClient = new ApiClient();
+
+        public IActionResult Index()
+        {
+            ProfileViewModel profileViewModel = new ProfileViewModel
+            (
+                pessoa: new Pessoa()
+                {
+                    PessoaID = 1,
+                    Nome = "Thainá",
+                    Email = "thaina@gmail.com",
+                    Login = "Thainá",
+                    Senha = "123456",
+                    DataNascimento = DateTimeOffset.Now
+                },
+                posts: ApiClient.GetPosts()
+            );
+            return View(profileViewModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
