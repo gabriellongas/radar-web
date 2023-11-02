d9fd696d820e00b3a14b7591305a8290f7ec3e4a
Lucas Savassa <lucaspsavassa@gmail.com>
2023-11-01 22:30:48 -0300

    Implement sign-up

--- Radar.Web/Controllers/CadastroController.cs
+++ Radar.Web/Controllers/CadastroController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Radar.Web.Api;
+using Radar.Web.Models;
 
 namespace Radar.Web.Controllers
 {
     public class CadastroController : Controller
     {
+        private readonly ApiClient _apiClient = new();
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Register(Pessoa pessoa)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Views/Cadastro/Index.cshtml", pessoa);
+            }
+
+            if (!_apiClient.PostPessoa(pessoa))
+            {
+                return View("Views/Shared/Error.cshtml");
+            }
+
+            return View("Views/Login/Index.cshtml");
         }
     }
 }
