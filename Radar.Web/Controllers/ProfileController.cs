38e269bb56dfbfb6e1b8d7a5adc215169a78d03d
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2023-11-20 11:29:21 -0300

    Implement settings page

--- Radar.Web/Controllers/ProfileController.cs
+++ Radar.Web/Controllers/ProfileController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Radar.Web.Api;
-using System.Diagnostics;
 
 namespace Radar.Web.Controllers
 {
@@ -17,16 +16,109 @@ namespace Radar.Web.Controllers
 
             if (id <= 0) id = LoginController.CurrentUserID;
 
-            ProfileViewModel profileViewModel = new (
-                pessoa: _apiClient.GetPessoa(id),
-                posts: _apiClient.GetPostsFromPessoa(LoginController.CurrentUserID, id)
-            );
+            ProfileViewModel profileViewModel = new()
+            {
+                Pessoa = _apiClient.GetPessoa(id),
+                Posts = _apiClient.GetPostsFromPessoa(LoginController.CurrentUserID, id),
+                IsMe = id == LoginController.CurrentUserID
+            };
 
             ViewBag.CurrentUserId = LoginController.CurrentUserID;
             ViewBag.Url = $"{ApiClient.Origin}{ApiClient.CurtidaPath}";
             ViewBag.Token = ApiClient.Token;
 
             return View(profileViewModel);
+        }
+
+        public IActionResult Settings()
+        {
+            PessoaReadDto currentMe = _apiClient.GetPessoa(LoginController.CurrentUserID);
+            Settings currentSettings = new()
+            {
+                Nome = currentMe.Nome,
+                Email = currentMe.Email,
+                Login = currentMe.Login,
+                Descricao = currentMe.Descricao,
+                DataNascimento = currentMe.DataNascimento,
+                NewPassword = null,
+                ConfirmPassword = null
+            };
+
+            SettingsViewModel viewModel = new()
+            {
+                Pessoa = currentMe,
+                Settings = currentSettings
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult Edit(SettingsViewModel newSettings)
+        {
+            PessoaReadDto currentMe = _apiClient.GetPessoa(LoginController.CurrentUserID);
+
+            if (!ModelState.IsValid)
+            {
+                SettingsViewModel viewModel = new()
+                {
+                    Pessoa = currentMe,
+                    Settings = newSettings.Settings
+                };
+
+                return View("Settings", viewModel);
+            }
+
+            PessoaLoginDto signIn = new()
+            {
+                Email = newSettings.Settings.Login,
+                Login = newSettings.Settings.Login,
+                Senha = newSettings.Settings.Senha
+            };
+
+            try
+            {
+                if (!_apiClient.ValidatePassword(signIn))
+                {
+                    ModelState.AddModelError("Settings.Senha", "Senha incorreta");
+
+                    SettingsViewModel viewModel = new()
+                    {
+                        Pessoa = currentMe,
+                        Settings = newSettings.Settings
+                    };
+
+                    return View("Settings", viewModel);
+                }
+
+                PessoaUpdateDto newMe = new()
+                {
+                    PessoaId = LoginController.CurrentUserID,
+                    Nome = newSettings.Settings.Nome,
+                    Email = newSettings.Settings.Email,
+                    Login = newSettings.Settings.Login,
+                    Descricao = newSettings.Settings.Descricao,
+                };
+
+                _apiClient.PutPessoa(newMe);
+
+                if (!string.IsNullOrWhiteSpace(newSettings.Settings.NewPassword))
+                {
+                    UpdatePasswordDto newMePassword = new()
+                    {
+                        PessoaId = LoginController.CurrentUserID,
+                        NewPassword = newSettings.Settings.NewPassword
+                    };
+
+                    _apiClient.UpdatePassword(newMePassword);
+                }
+
+                return RedirectToAction("Index", "Profile", new { id = LoginController.CurrentUserID });
+            }
+            catch (Exception)
+            {
+                return View("Views/Shared/Error.cshtml", new ErrorViewModel());
+            }
+            
         }
     }
 }
--- Radar.Web/Models/Dto/Pessoa/PessoaUpdateDto.cs
+++ Radar.Web/Models/Dto/Pessoa/PessoaUpdateDto.cs
@@ -6,7 +6,5 @@ public class PessoaUpdateDto
     public string Nome { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Login { get; set; } = null!;
-    public string Senha { get; set; } = null!;
     public string? Descricao { get; set; }
-    public DateTime DataNascimento { get; set; }
 }
