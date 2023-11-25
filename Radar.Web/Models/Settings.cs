6abd523326aaf535e2ab3bb432bb84868a7c6a8c
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2023-11-25 10:52:59 -0300

    Fix field display names

--- Radar.Web/Models/Settings.cs
+++ Radar.Web/Models/Settings.cs
@@ -7,14 +7,14 @@ namespace Radar.Web.Models
         [MaxLength(50, ErrorMessage = "A senha deve ter no máximo 255 caracteres")]
         [MinLength(8, ErrorMessage = "A senha deve ter no mínimo 8 caracteres")]
         [DataType(DataType.Password)]
-        [Display(Name = "Senha Atual")]
+        [Display(Name = "Nova senha")]
         public string? NewPassword { get; set; }
 
         [MaxLength(50, ErrorMessage = "A senha deve ter no máximo 255 caracteres")]
         [MinLength(8, ErrorMessage = "A senha deve ter no mínimo 8 caracteres")]
         [DataType(DataType.Password)]
         [Compare("NewPassword", ErrorMessage = "As senhas não coincidem")]
-        [Display(Name = "Confirmar Senha")]
+        [Display(Name = "Confirmar senha")]
         public string? ConfirmPassword { get; set; }
     }
 }
