03722fbf94eda5a7c0c371f213caacd8c6a7c1df
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2023-11-26 13:59:19 -0300

    Improve error handling

--- Radar.Web/Models/ViewModels/PublishPopupViewModel.cs
+++ Radar.Web/Models/ViewModels/PublishPopupViewModel.cs
@@ -11,11 +11,13 @@ namespace Radar.Web.Models.ViewModels
         [Display(Name = "Local")]
         public string? SelectedLocalName { get; set; }
 
+        [Required(ErrorMessage = "O conteúdo é obrigatório")]
         [MaxLength(255, ErrorMessage = "O conteúdo não pode ter mais que 255 caracteres")]
         [Display(Name = "Conteúdo")]
         public string? Conteudo { get; set; }
 
         [Range(1, 5, ErrorMessage = "A avaliação deve ser um número entre 1 e 5")]
+        [Required(ErrorMessage = "A avaliação é obrigatória")]
         [Display(Name = "Avaliação")]
         public int? Avaliacao { get; set; }
     }
